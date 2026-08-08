8d1475dcfc68a200a7eb83617ed715e74118d5ae
MarathonTrainer Developer <dev@maratontrener.no>
2026-08-08 21:23:14 +0200

    Bump Android app version to 4.94

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 23
         targetSdk = 35
 
-        versionCode = 493
-        versionName = "4.93"
+        versionCode = 494
+        versionName = "4.94"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
